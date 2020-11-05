02937490359b65ca3f01f31d4399580c25c14b35
Sharkified Creator <1693useroscar@gmail.com>
2020-11-05 20:21:11 +0100

    Changes to Project Shark

---
+++ School/Shark Project/Assets/Scripts/MovementTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementTest : MonoBehaviour
+{
+
+    public float movementAcceleration = 1.0f;
+    public float movementSpeed = 1.0f;
+    public float horizontalFloatInput = Input.GetAxis("Horizontal");
+    public float verticalFloatInput = Input.GetAxis("Vertical");
+        // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(horizontalFloatInput, 0, verticalFloatInput) * movementSpeed * Time.deltaTime);  
+    }
+}

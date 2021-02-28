4cbcf3a0e29a6aca7b215ef8e65647a7bd9af7f5
Sharkified Creator <1693useroscar@gmail.com>
2021-02-28 23:07:01 +0100

    Update to Proof of Concept
    Further prototypes of Trebuchet.

---
+++ School/PMwithAgileSprint3GameProofOfConcept/Assets/CenterOfMassReadjustment.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CenterOfMassReadjustment : MonoBehaviour
+{
+    [SerializeField] protected Rigidbody2D rb;
+    private Vector2 com;
+    // Start is called before the first frame update
+    void Start()
+    {
+        com = new Vector2(0f, 0f);
+        rb.centerOfMass = com;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ School/PMwithAgileSprint3GameProofOfConcept/Assets/PullingLever.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PullingLever : MonoBehaviour
+{
+    private GameObject gO;
+    [SerializeField] protected float frequency = 1f;
+    private SpringJoint2D sJ;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gO = GameObject.FindGameObjectWithTag("Spring");
+        sJ = gO.GetComponent<SpringJoint2D>();
+        sJ.frequency = frequency;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PullTensionLever()
+    {
+        frequency += 0.5f;
+        sJ.frequency = frequency;
+
+    }
+}
